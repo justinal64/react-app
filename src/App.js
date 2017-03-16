5194586ea27cc7f27dd7a0dfd8569f33a99afaaa
justin leggett <justin@codingrookie.com>
2017-03-16 09:26:27 -0500

    completed tutorial 5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+    // super gives us access to the keyword 'this'
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state txt',
+            cat: 0
+        }
+    }
+    // custom state on our component
+    update(e) {
+        this.setState({txt: e.target.value})
+    }
     render(){
-        let txt = this.props.txt
-        let cat = this.props.cat
         return (
             <div>
-                <h1>txt = {txt}</h1>
-                <h1>cat = {cat}</h1>
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h1>{this.state.txt} - {this.state.cat}</h1>
             </div>
         )
     }
-}
-
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-
-// Default value for txt
-App.defaultProps = {
-    txt: "This is the default text"
 }
 
 export default App
