e419fb9f0fced230489353fcd92a903e45bc3488
justin leggett <justin@codingrookie.com>
2017-03-16 12:02:20 -0500

    completed exercise 7

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
 
 class App extends React.Component {
-    // super gives us access to the keyword 'this'
-    constructor() {
-        super();
-        this.state = {
-            txt: 'this is the state txt'
-        }
-    }
-    // custom state on our component
-    update(e) {
-        this.setState({txt: e.target.value})
-    }
     render(){
-        return (
-            <div>
-                <h1>{this.state.txt}</h1>
-                <Widget update={this.update.bind(this)} />
-                <Widget update={this.update.bind(this)} />
-                <Widget update={this.update.bind(this)} />
-            </div>
-        )
+        return <button> I <Heart /> React</button>
+
     }
 }
 
-const Widget = (props) =>
-        <input type="text" onChange={props.update} />
+class Heart extends React.Component {
+    render() {
+        return <span>&hearts;</span>
+    }
 
+}
+
+const Button = (props) => <button>{props.children}</button>
 export default App
