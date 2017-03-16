ac44f9f0cb9cdec95cada51c5c795e37f6e6d332
justin leggett <justin@codingrookie.com>
2017-03-16 15:43:24 -0500

    using react rf to access an input field

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {currentEvent: '---'}
-        this.update = this.update.bind(this)
+        this.state = {a: ''}
     }
     update(e) {
-        this.setState({currentEvent: e.type})
+        this.setState({
+            a: this.refs.a.value,
+            b: this.refs.b.value
+        })
     }
     render(){
         return (
             <div>
-                <textarea
-                    onKeyPress={this.update}
-                    onCopy={this.update}
-                    onCut={this.update}
-                    onPaste={this.update}
-                    onFocus={this.update}
-                    onBlur={this.update}
-                    onDoubleClick={this.update}
-                    cols="30"
-                    rows="10" />
-                <h1>{this.state.currentEvent}</h1>
+                <input
+                    ref="a"
+                    type="text"
+                    onChange={this.update.bind(this)}
+                /> {this.state.a}
+                <hr />
+                <input
+                    ref="b"
+                    type="text"
+                    onChange={this.update.bind(this)}
+                /> {this.state.b}
             </div>
         )
     }
